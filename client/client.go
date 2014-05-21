4d89f702905bb6e62b02ad82a860e2d882dfa626
emmanuel <eflorac@intellique.com>
2014-05-21 16:13:23 +0200

    binary exec gofmt'd after 1.2.2 upgrade

--- client/client.go
+++ client/client.go
@@ -1,27 +1,26 @@
 package main
 
 import (
-	"log"
-	"fmt"
 	"code.google.com/p/go.net/websocket"
+	"fmt"
+	"log"
 )
 
-func main () {
+func main() {
 	origin := "http://localhost/"
 	url := "ws://localhost:8080/ws"
 	ws, err := websocket.Dial(url, "", origin)
 	if err != nil {
-    	log.Fatal(err)
+		log.Fatal(err)
 	}
 
 	var msg = make([]byte, 512)
 	var n int
 
 	if n, err = ws.Read(msg); err != nil {
-    	log.Fatal(err)
+		log.Fatal(err)
 	}
 	fmt.Printf("Received: %s.\n", msg[:n])
-	
+
 	err = ws.Close()
 }
-
--- server/server.go
+++ server/server.go
@@ -1,44 +1,42 @@
 package main
 
 import (
-	"net/http"
-	"log"
 	"code.google.com/p/go.net/websocket"
-	"time"
+	"log"
 	"math"
+	"net/http"
 	"strconv"
+	"time"
 )
 
 func main() {
 	log.Println("Starting Server...")
 
-	http.Handle("/ws", websocket.Handler(handler));
+	http.Handle("/ws", websocket.Handler(handler))
 
-	err := http.ListenAndServe("127.0.0.1:8080", nil);
+	err := http.ListenAndServe("127.0.0.1:8080", nil)
 
 	if err != nil {
-		log.Fatal("ListenAndServe: ", err )
+		log.Fatal("ListenAndServe: ", err)
 	}
 }
 
 // global var: all clients see the same curve
 var x float64 = 0
 
-
 func handler(ws *websocket.Conn) {
-	
+
 	for {
 		if x >= 2*math.Pi {
 			x = 0
 		} else {
 			x += 0.05
 		}
-		
-		time.Sleep(500*1000*1000) // sleep for 500ms (Sleep takes nanoseconds)
+
+		time.Sleep(500 * 1000 * 1000) // sleep for 500ms (Sleep takes nanoseconds)
 
 		msg := strconv.FormatFloat(math.Sin(x), 'g', 10, 64)
 		log.Printf("%v sending: %v\n", ws, msg)
-		websocket.Message.Send( ws, msg )
+		websocket.Message.Send(ws, msg)
 	}
 }
-
